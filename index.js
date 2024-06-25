43ff15e3a65c4a314550d2b36e57e9827ddb52d3
OMI786I <sohrawardy1998@gmail.com>
2024-06-26 02:14:01 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -9,11 +10,10 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
-//reset-Assignment-10
-//IPYFeolKolIJo4QA
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ymyoldm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+console.log(process.env.DB_USER);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,6 +27,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const dataCollection = client
+      .db("reset-Assignment-10")
+      .collection("tourist-spot-section");
+
+    //for reading from mongodb
+
+    app.get("/tourist-spot-section", async (req, res) => {
+      const cursor = dataCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +47,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
